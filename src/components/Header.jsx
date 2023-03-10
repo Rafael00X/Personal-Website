cd37f8abff5e0fbf9a3bac28574f17f650cc71f5
unknown <amsubhadeep@gmail.com>
2023-03-10 17:23:12 +0530

    Changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function Header() {
   return (
     <section id="header">
-      <div>
-        <h1>I'm Subhadeep</h1>
-        <h2>a programmer</h2>
+      <div className="display">
+        <div style={{ maxWidth: "600px", margin: "auto", marginTop: "250px" }}>
+          <h1>I'm Subhadeep</h1>
+          <h2>a programmer</h2>
+        </div>
       </div>
     </section>
   );
